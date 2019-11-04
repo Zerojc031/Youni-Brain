ac72150584b6047bad7486c2abcfab98d85c6162
Zerojc031 <champion952@outlook.com>
2019-11-04 19:20:35 +0800

    统计小助手功能增强
    增加多种可选项

--- miniprogram/program/pages/index/MapDIY/MapDIY.js
+++ miniprogram/program/pages/index/MapDIY/MapDIY.js
@@ -13,34 +13,42 @@ Page({
    * 页面的初始数据
    */
   data: {
+    presentPage: 1,
     index: 5,
     array: [{
         id: 1,
         value: 30,
-        color: '#00BFFF'
+        color: '#00BFFF',
+        weight: '20%'
       },
       {
         id: 2,
         value: 30,
-        color: '#00FA9A'
+        color: '#00FA9A',
+        weight: '20%'
       },
       {
         id: 3,
         value: 30,
-        color: '#FF7256'
+        color: '#FF7256',
+        weight: '20%'
       },
       {
         id: 4,
         value: 30,
-        color: '#FFF68F'
+        color: '#FFF68F',
+        weight: '20%'
       },
       {
         id: 5,
         value: 30,
-        color: '#EE82EE'
+        color: '#EE82EE',
+        weight: '20%'
       },
     ],
     range: 50,
+    switchStatus: 1,
+    paintType:1,
   },
 
   /**
@@ -96,12 +104,13 @@ Page({
    * 用户点击右上角分享
    */
   onShareAppMessage: function() {
-    
+
   },
 
   draw: function() {
     let count = 0,
-      thisAngle = 0;
+      thisAngle = 0,
+      switchPaint = '';
     ctx.rect(0, 0, 360, 360)
     ctx.setLineWidth(0)
     ctx.setFillStyle('white')
@@ -112,19 +121,22 @@ Page({
     for (let i = 0; i < this.data.index; i++) {
       if (this.data.array[i].value == 0) continue;
       angle[i] = 2 * mathPi * this.data.array[i].value / count;
-      this.paint(this.data.array[i].value, thisAngle, thisAngle + angle[i], this.data.array[i].color);
+      let tempWeight = this.data.array[i].value / count * 100;
+      tempWeight = tempWeight.toFixed(0) + '%';
+      if (this.data.switchStatus == 1) switchPaint = this.data.array[i].value;
+      else if (this.data.switchStatus == 2) switchPaint = tempWeight;
+      if (this.data.paintType == 1) this.paint1(switchPaint, thisAngle, thisAngle + angle[i], this.data.array[i].color); else if (this.data.paintType == 2) this.paint2(switchPaint, thisAngle, thisAngle + angle[i], this.data.array[i].color);
       thisAngle += angle[i];
       // console.log(thisAngle + angle[i] + this.data.array[i].color)
     }
     ctx.draw();
   },
 
-  paint: function(value, beginAngle, endAngle, color) {
+  paint1: function(value, beginAngle, endAngle, color) {
     ctx.beginPath()
     ctx.arc(center, center, 2 * unitR, beginAngle, endAngle)
     ctx.lineTo(center + unitR * Math.cos(endAngle), center + unitR * Math.sin(endAngle))
     ctx.arc(center, center, unitR, endAngle, beginAngle, true)
-    // ctx.moveTo(center + unitR * Math.cos(beginAngle), center + unitR * Math.sin(beginAngle))
     ctx.lineTo(center + 2 * unitR * Math.cos(beginAngle), center + 2 * unitR * Math.sin(beginAngle))
     ctx.closePath()
     ctx.setFillStyle(color)
@@ -133,13 +145,39 @@ Page({
     ctx.fill()
     ctx.setFontSize(20)
     ctx.setFillStyle('#5C5C5C')
-    let middleAngle = (endAngle + beginAngle) / 2,
-      k = middleAngle * 2 / Math.PI
-    // ctx.fillText(value, center + 2 * (unitR + 7) * Math.cos(middleAngle), center + 2 * (unitR + 15) * Math.sin(middleAngle))
-    if (k <= 1) ctx.fillText(value, center + 2 * (unitR + 8) * Math.cos(middleAngle) - 10, center + 2 * (unitR + 8) * Math.sin(middleAngle))
-    else if (k < 2) ctx.fillText(value, center + 2 * (unitR + 8) * Math.cos(middleAngle) - 10, center + 2 * (unitR + 8) * Math.sin(middleAngle))
-    else if (k < 3) ctx.fillText(value, center + 2 * (unitR + 7) * Math.cos(middleAngle) - 10, center + 2 * (unitR + 8) * Math.sin(middleAngle) + 10)
-    else ctx.fillText(value, center + 2 * (unitR + 8) * Math.cos(middleAngle) - 10, center + 2 * (unitR + 8) * Math.sin(middleAngle) + 10)
+    if (this.data.switchStatus > 0) {
+      //渲染数字
+      let middleAngle = (endAngle + beginAngle) / 2,
+        k = middleAngle * 2 / Math.PI
+      if (k <= 1) ctx.fillText(value, center + 2 * (unitR + 8) * Math.cos(middleAngle) - 10, center + 2 * (unitR + 8) * Math.sin(middleAngle))
+      else if (k < 2) ctx.fillText(value, center + 2 * (unitR + 8) * Math.cos(middleAngle) - 10, center + 2 * (unitR + 8) * Math.sin(middleAngle))
+      else if (k < 3) ctx.fillText(value, center + 2 * (unitR + 7) * Math.cos(middleAngle) - 15, center + 2 * (unitR + 8) * Math.sin(middleAngle) + 10)
+      else ctx.fillText(value, center + 2 * (unitR + 8) * Math.cos(middleAngle) - 10, center + 2 * (unitR + 8) * Math.sin(middleAngle) + 10)
+    }
+    ctx.stroke()
+  },
+
+  paint2: function(value, beginAngle, endAngle, color) {
+    ctx.beginPath()
+    ctx.arc(center, center, 2 * unitR, beginAngle, endAngle)
+    ctx.lineTo(center, center)
+    ctx.lineTo(center + 2 * unitR * Math.cos(beginAngle), center + 2 * unitR * Math.sin(beginAngle))
+    ctx.closePath()
+    ctx.setFillStyle(color)
+    ctx.setStrokeStyle(color)
+    ctx.setLineWidth(0)
+    ctx.fill()
+    ctx.setFontSize(20)
+    ctx.setFillStyle('#5C5C5C')
+    if (this.data.switchStatus > 0) {
+      //渲染数字
+      let middleAngle = (endAngle + beginAngle) / 2,
+        k = middleAngle * 2 / Math.PI
+      if (k <= 1) ctx.fillText(value, center + 2 * (unitR + 8) * Math.cos(middleAngle) - 10, center + 2 * (unitR + 8) * Math.sin(middleAngle))
+      else if (k < 2) ctx.fillText(value, center + 2 * (unitR + 8) * Math.cos(middleAngle) - 10, center + 2 * (unitR + 8) * Math.sin(middleAngle))
+      else if (k < 3) ctx.fillText(value, center + 2 * (unitR + 7) * Math.cos(middleAngle) - 15, center + 2 * (unitR + 8) * Math.sin(middleAngle) + 10)
+      else ctx.fillText(value, center + 2 * (unitR + 8) * Math.cos(middleAngle) - 10, center + 2 * (unitR + 8) * Math.sin(middleAngle) + 10)
+    }
     ctx.stroke()
   },
 
@@ -186,5 +224,78 @@ Page({
         console.log('complete', res)
       }
     }, this)
-  }
+  },
+
+  none: function() {},
+
+  toItems: function() {
+    if (this.data.presentPage != 0) this.setData({
+      presentPage: 0
+    })
+  },
+
+  toData: function() {
+    if (this.data.presentPage != 1) this.setData({
+      presentPage: 1
+    })
+  },
+
+  toSwitch: function() {
+    if (this.data.presentPage != 2) this.setData({
+      presentPage: 2
+    })
+  },
+
+  toHelp: function() {
+    if (this.data.presentPage != 3) this.setData({
+      presentPage: 3
+    })
+  },
+
+  switchValueChange: function(e) {
+    if (e.detail.value == true) {
+      this.setData({
+        switchStatus: 1
+      })
+      this.draw()
+    } else {
+      this.setData({
+        switchStatus: 0
+      })
+      this.draw()
+    }
+  },
+
+  switchPercentageChange: function(e) {
+    if (e.detail.value == true) {
+      this.setData({
+        switchStatus: 2
+      })
+      this.draw()
+    } else {
+      this.setData({
+        switchStatus: 0
+      })
+      this.draw()
+    }
+  },
+
+  toPaint1:function(){
+    if(this.data.paintType!=1){
+      this.setData({
+        paintType:1
+      })
+      this.draw()
+    }
+  },
+
+  toPaint2: function () {
+    if (this.data.paintType != 2) {
+      this.setData({
+        paintType: 2
+      })
+      this.draw()
+    }
+  },
+  
 })
